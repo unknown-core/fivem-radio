a177c1a1d413cbf6248a257621e7eacd5498dcc5
Deihim Poornader <deihim007@gmail.com>
2022-05-22 13:49:24 +0430

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ description 'This resource allows you to integrate your own radios in place of t
 version '2.0.0'
 
 -- Example custom radios
-supersede_radio 'RADIO_36_AUDIOPLAYER' { url = 'https://irsv.upmusics.com/Up-Song/Aron%20Afshar%20%7C%20Janam%20Bash%20%7Basli%7D%20(320).mp3', volume = 0.2, name = 'Radio Farda' }
+supersede_radio 'RADIO_36_AUDIOPLAYER' { url = 'https://irsv.upmusics.com/Up-Song/Aron%20Afshar%20%7C%20Janam%20Bash%20%7Basli%7D%20(320).mp3', volume = 0.3, name = 'Radio Farda' }
 
 files {
 	'index.html'
