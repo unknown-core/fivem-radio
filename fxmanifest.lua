1ebb6b2a4e63b0cba42199139a50985e0a4d14a2
Deihim Poornader <deihim007@gmail.com>
2022-05-22 14:15:56 +0430

    Add radio sepehr

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,7 @@ description 'This resource allows you to integrate your own radios in place of t
 version '2.0.0'
 
 -- Example custom radios
+supersede_radio 'RADIO_35_DLC_HEI4_MLR' { url = 'https://sepehr.irib.ir/frame/t/rn-iran', volume = 0.2, name = 'Radio Sepehr' }
 supersede_radio 'RADIO_36_AUDIOPLAYER' { url = 'https://n06.radiojar.com/cp13r2cpn3quv?rj-ttl=5&rj-tok=AAABgOsLjHsAyGxQasXFdTV0IQ', volume = 0.2, name = 'Radio Farda' }
 
 files {
