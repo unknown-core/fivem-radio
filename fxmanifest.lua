27ae9ddc67fd6ae0a52d4a6a0e5a21ed205fdc04
Deihim Poornader <deihim007@gmail.com>
2022-05-22 13:42:27 +0430

    Add radio farda to channel 36

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,7 @@ description 'This resource allows you to integrate your own radios in place of t
 version '2.0.0'
 
 -- Example custom radios
-supersede_radio 'RADIO_02_POP' { url = 'https://n06.radiojar.com/cp13r2cpn3quv?rj-ttl=5&rj-tok=AAABgOsLjHsAyGxQasXFdTV0IQ', volume = 0.2, name = 'Radio Farda' }
-supersede_radio 'RADIO_03_HIPHOP_NEW' { url = 'http://stream.radioreklama.bg/nrj.ogg', volume = 0.2 }
+supersede_radio 'RADIO_36_AUDIOPLAYER' { url = 'https://n06.radiojar.com/cp13r2cpn3quv?rj-ttl=5&rj-tok=AAABgOsLjHsAyGxQasXFdTV0IQ', volume = 0.2, name = 'Radio Farda' }
 
 files {
 	'index.html'
